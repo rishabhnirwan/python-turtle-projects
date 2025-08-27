f694223629bb674d04523b264101f8eb69248c7e
polymethical <polymethical@gmail.com>
2025-08-27 10:24:33 +0530

    Added comments.

--- p5.py
+++ p5.py
@@ -1,7 +1,8 @@
 import turtle
 from random import randint
 
-turtle.colormode(255)
+turtle.colormode(255) #set colormode to 255
+
 tim = turtle.Turtle() #inistialise turtle obj
 
 #set properties for tim
@@ -11,17 +12,15 @@ tim.speed(0)
 
 screen = turtle.Screen() #initialise screen object
 
+#number of circles
 n =50
 
 for i in range(0,n):
+    #pick random color for tim 
     rand_color = (randint(0,255), randint(0,255), randint(0,255))
     tim.color(rand_color)
-    tim.circle(200)
-    tim.left(360 / n)
-
-
-
-
+    tim.circle(200) #draw circle
+    tim.left(360 / n) #rotate
 
 
 screen.exitonclick()
